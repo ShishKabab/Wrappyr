19c774491178ec8d70e415c509cf7c6d0fee9c75
Vincent den Boer <vincent@shishkabab.net>
2011-10-27 20:36:36 +0200

    Removed autodoc dependency for documentation

--- docs/creating_package_classes.rst
+++ docs/creating_package_classes.rst
@@ -14,8 +14,104 @@ Create a Package class is the preferred way to customize to generation of a Pyth
 
 To create a Package, subclass from :class:`wrappyr.Package`. Below, I will describe each method you can override and how you can use it.
 
-.. autoclass:: wrappyr.Package
-   :members: get_header_export, get_source_export, get_ctypes_export,
-             get_source_language, get_source_header_path, get_generated_header_path,
-             get_generated_source_path, process_code_import, process_ctypes_structure,
-             source_header_name
+.. class:: wrappyr.Package
+
+    Base class for all Wrappyr packages.
+
+    Overriding the methods of this class is the preferred way to hook into
+    the wrappyr/generate.py script. To use your Package subclass, pass the
+    dotted path to it to the ``--package`` option of the wrappyr/generate.py
+    script.
+
+    .. method:: get_header_export()
+
+        Returns class used to generate the header of the C API.
+
+        You will almost always want to return a subclass of
+        :class:`wrappyr.code_data_conversion.HeaderExport`.
+        The main reason to do this is to use a custom export filter
+        controlling which classes and functions get exported to the Python API.
+
+    .. method:: get_source_export()
+
+        Returns class used to generate the source of the C API.
+
+        You will almost always want to return a subclass of
+        :class:`wrappyr.code_data_conversion.SourceExport`.
+        The main reasons to do this are to use a custom export filter
+        controlling which classes and functions get exported to the
+        Python API and to add custom includes to the source of the C API.
+
+    .. method:: get_ctypes_export()
+
+        Returns class used to generate the XML describing the Python API.
+
+        You will almost always want to return a subclass of
+        :class:`wrappyr.code_data_conversion.CtypesExport`.
+        Such a subclass can have a custom filter used to control which classes
+        and functions get exported to the Python API. You can also use this
+        to provide a basic namespace to package mapping.
+
+    .. method:: get_source_language()
+
+        Returns which language your C/C++ is using as a string.
+
+        Return either lowercase ``c`` or ``c+``.
+
+    .. method:: get_source_header_path()
+
+        Returns the path of the header used to generate the Python API.
+
+        The default implementation searches /usr/local/include and /usr/include
+        for the header file in the :attr:`source_header_name` attribute.
+
+    .. method:: get_generated_header_path()
+
+        Returns the path where to write the header of the generated C API.
+
+        By default this generates a random temporary file ending in .h and
+        stores it name in :attr:`generated_wrapper_prefix` so
+        :attr:`get_generated_source_path` can use the same name
+        ending in .cpp .
+
+    .. method:: get_generated_source_path()
+
+        Returns the path where to write the source of the generated C API.
+
+        By default this generates a random temporary file ending in .cpp and
+        stores it name in :attr:`generated_wrapper_prefix` so
+        :attr:`get_generated_header_path` can use the same name
+        ending in .h .
+
+    .. method:: process_code_import(importer)
+
+        Preprocess the imported code data.
+
+        You can use this to store information about the C/C++ code. See the
+        Box2D package in packages/Box2D.py which uses this to store the location
+        of classes, which it uses in :attr:`process_ctypes_structure` to
+        organize the final Python API into packages.
+
+    .. method:: process_ctypes_structure(structure)
+
+        Preprocess the final Python API imported from XML.
+
+        This method is passed an argument named *structure*,
+        which is an instance of the class
+        :class:`wrappyr.ctypes_api_builder.structure.CTypesStructure`.
+
+        Use this to do both changes that are cosmetic and changes that are
+        necessary for the Python API to work. Cosmetic changes include
+        converting method names to PEP8 convention and organizing classes
+        into packages. Necessary changes include renaming arguments that are
+        reserved Python keywords and removing ambiguous overloads.
+
+        See :doc:`ctypes_api_generation` for the docs you'll need and some examples.
+        Also, see the Box2D package in packages/Box2D.py for a real life example.
+
+    .. attribute:: source_header_name
+
+        Property used by the default implementation of get_source_header_path to
+        find the header file used as a starting point for generating everything
+        necessary for the Python API. You can set this to something
+        like ``Box2D/Box2D.h``.
--- wrappyr/__init__.py
+++ wrappyr/__init__.py
@@ -20,18 +20,6 @@ def get_version():
 
 class HeaderNotFound(Exception): pass
 class Package(object):
-    """Base class for all Wrappyr packages.
-
-    Overriding the methods of this class is the preferred way to hook into
-    the wrappyr/generate.py script. To use your Package subclass, pass the
-    dotted path to it to the ``--package`` option of the wrappyr/generate.py
-    script.
-    """
-
-    #: Property used by the default implementation of get_source_header_path to
-    #: find the header file used as a starting point for generating everything
-    #: necessary for the Python API. You can set this to something
-    #: like ``Box2D/Box2D.h``.
     source_header_name = None
 
     #: Property used by default implementation of `get_generated_header_path`
@@ -46,122 +34,40 @@ class Package(object):
     filter_class = None
 
     def get_header_export(self):
-        """Returns class used to generate the header of the C API.
-
-        You will almost always want to return a subclass of
-        :class:`wrappyr.code_data_conversion.HeaderExport`.
-        The main reason to do this is to use a custom export filter
-        controlling which classes and functions get exported to the Python API.
-        """
-
         return HeaderExport(filter_class = self.filter_class)
 
     def get_source_export(self):
-        """Returns class used to generate the source of the C API.
-
-        You will almost always want to return a subclass of
-        :class:`wrappyr.code_data_conversion.SourceExport`.
-        The main reasons to do this are to use a custom export filter
-        controlling which classes and functions get exported to the
-        Python API and to add custom includes to the source of the C API.
-        """
-
         return SourceExport(filter_class = self.filter_class)
 
     def get_ctypes_export(self):
-        """Returns class used to generate the XML describing the Python API.
-
-        You will almost always want to return a subclass of
-        :class:`wrappyr.code_data_conversion.CtypesExport`.
-        Such a subclass can have a custom filter used to control which classes
-        and functions get exported to the Python API. You can also use this
-        to provide a basic namespace to package mapping.
-        """
-
         return CtypesExport(filter_class = self.filter_class)
 
     def get_source_language(self):
-        """Returns which language your C/C++ is using as a string.
-
-        Returns either lowercase ``c`` or ``c+``.
-        """
-
         return "c++"
 
     def get_preferred_code_data_backend(self):
-        """Returns which backend to use when exporting.
-
-        Valid return values are ``gccxml`` and ``clang``.
-        """
-
         return "gccxml"
 
     def get_source_header_path(self):
-        """Returns the path of the header used to generate the Python API.
-
-        The default implementation searches /usr/local/include and /usr/include
-        for the header file in the :attr:`source_header_name` attribute.
-        """
-
         if self.source_header_name:
             return self.find_header(self.source_header_name)
 
     def get_generated_header_path(self):
-        """Returns the path where to write the header of the generated C API.
-
-        By default this generates a random temporary file ending in .h and
-        stores it name in :attr:`generated_wrapper_prefix` so
-        :attr:`get_generated_source_path` can use the same name
-        ending in .cpp .
-        """
-
         if not self.generated_wrapper_prefix:
             self.generated_wrapper_prefix = mkstemp()[1]
 
         return self.generated_wrapper_prefix + ".h"
 
     def get_generated_source_path(self):
-        """Returns the path where to write the source of the generated C API.
-
-        By default this generates a random temporary file ending in .cpp and
-        stores it name in :attr:`generated_wrapper_prefix` so
-        :attr:`get_generated_header_path` can use the same name
-        ending in .h .
-        """
-
         if not self.generated_wrapper_prefix:
             self.generated_wrapper_prefix = mkstemp()[1]
 
         return self.generated_wrapper_prefix + ".cpp"
 
     def process_code_import(self, importer):
-        """Preprocess the imported code data.
-
-        You can use this to store information about the C/C++ code. See the
-        Box2D package in packages/Box2D.py which uses this to store the location
-        of classes, which it uses in :attr:`process_ctypes_structure` to
-        organize the final Python API into packages.
-        """
-
         pass
 
     def process_ctypes_structure(self, structure):
-        """Preprocess the final Python API imported from XML.
-
-        This method is passed an argument named *structure*,
-        which is an instance of the class
-        :class:`wrappyr.ctypes_api_builder.structure.CTypesStructure`.
-
-        Use this to do both changes that are cosmetic and changes that are
-        necessary for the Python API to work. Cosmetic changes include
-        converting method names to PEP8 convention and organizing classes
-        into packages. Necessary changes include renaming arguments that are
-        reserved Python keywords and removing ambiguous overloads.
-
-        See :doc:`ctypes_api_generation` for the docs you'll need and some examples.
-        Also, see the Box2D package in packages/Box2D.py for a real life example.
-        """
-
         pass
 
     def find_header(self, name):
